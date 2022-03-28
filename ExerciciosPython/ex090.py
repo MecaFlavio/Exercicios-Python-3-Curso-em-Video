634ec13b0c798138b847dd9faec1901521dfed1b
Flavio A. Pereira <flavio.metodista@hotmail.com>
2022-03-28 06:29:37 -0300

    Exercícios 090 e 091 sobre dicionários

--- ExerciciosPython/ex090.py
+++ ExerciciosPython/ex090.py
@@ -2,3 +2,15 @@
 # guardando também a situação num dicionário. No final,
 # mostre o conteúdo de estrutura na tela.
 
+dicionário = {'nome': str(input('Nome: ')), 'media': float(input('Media de: '))}
+if dicionário['media'] < 5:
+    dicionário['situação'] = 'Reprovado'
+elif dicionário['media'] < 7:
+    dicionário['situação'] = 'Recuperação'
+else:
+    dicionário['situação'] = 'Aprovado'
+print('-=' * 30)
+for k, v in dicionário.items():
+    print(f'{k}: {v}')
+
+
--- ExerciciosPython/ex091.py
+++ ExerciciosPython/ex091.py
@@ -1,3 +1,21 @@
 # Faça um programa onde 4 jogadores jogue um dado e tenham resultados aleatórios;
 # guarde todos esses resultados num dicionário. No final, coloque esse dicionário em ordem,
 # sabendo que o vencedor tirou o maior número no dado.
+from random import randint
+from time import sleep
+from operator import itemgetter  # lib que manipula itens do dicionario
+lista = []
+lista2 = []
+ranking = []
+dicionário = {'jog1': randint(1, 6), 'jog2': randint(1, 6), 'jog3': randint(1, 6),
+              'jog4': randint(1, 6)}
+# criando lista com itemgetter para organizar keys e valores dos dicionários.
+ranking = sorted(dicionário.items(), key=itemgetter(1), reverse=True)
+print(type(ranking))
+for k, v in dicionário.items():
+    print(f' O {k} tirou {v} no dado')
+    sleep(1)
+print('=' * 10, 'Ranking', '=' * 10)
+for i, v in enumerate(ranking):
+    print(f'{i + 1}ª lugar: {v[0]} com {v[1]}.')
+    sleep(1)
